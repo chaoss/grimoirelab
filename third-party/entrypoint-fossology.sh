fd10153fb0ed63401a375b47f42e5adb3befeec2
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2020-04-14 01:39:01 +0530

    [docker/full-3p] update the link for fossology source
    The existing fossology source repository is deleted
    which is causing the build to fail (#306).
    This commit updates it with the latest source link.
    
    Signed-off-by: Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>

--- third-party/entrypoint-fossology.sh
+++ third-party/entrypoint-fossology.sh
@@ -3,10 +3,10 @@
 echo "Starting container:" $(hostname)
 
 echo "Getting FOSSology source package..."
-dget -u https://mentors.debian.net/debian/pool/main/f/fossology/fossology_3.6.0-1.dsc
+dget -u http://mirrors.kernel.org/fossology/releases/3.0.0/debian/7.0/fossology_3.0.0-1.dsc
 
 echo "Building packages..."
-cd fossology-3.6.0
+cd fossology-3.0.0
 /usr/bin/debuild -us -uc
 echo "If FOSSology packages were built, look for them where you mounted /build"
 
